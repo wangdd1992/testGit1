ea8edb47fea45c58e40585d9fdf1219fe1441b55
wangdd1992 <www.1695786738@qq.com>
2020-05-11 10:56:13 +0800

    000

---
+++ Git1/Git1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Git1
+//
+//  Created by Wangdandan on 2020/5/11.
+//  Copyright © 2020 Wangdd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Git1/Git1/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Git1
+//
+//  Created by Wangdandan on 2020/5/11.
+//  Copyright © 2020 Wangdd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
